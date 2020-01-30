0ef620396e4c6a5db8943ab2ca6fe3c03093dd5c
tagine <rana@ranafayez.com>
2020-01-30 16:41:28 -0500

    ore data points added

--- Develop/index.js
+++ Develop/index.js
@@ -3,65 +3,62 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 // const questions = ["What's your favorite color?", "What's your GitHub username?"
-  
-// ];
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
-  .then(({ username }) => {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    axios.get(queryUrl).then(res => {
-      const repoNames = res.data.map(repo => repo.name + "\n");
-      fs.writeFile("repos.pdf", repoNames, (err, data) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log(
-          `Everything went super great. You did it. Your the best, ever! You saved ${repoNames.length}`
-        );
-      });
-    });
-  });
+// ];
 
 // * The application generates a PDF resume from the user provided GitHub profile.
 
-// The PDF will be populated with the following:
+inquirer
+    .prompt({
+        message: "Enter your GitHub username",
+        name: "username"
+    })
+    .then(({ username }) => {
+        const queryUrl = `https://api.github.com/users/${username}`;
+        axios.get(queryUrl).then(res => {
 
-// * Profile image
-// * User name
+            // The PDF will be populated with the following:
+
+        // * Profile image
+        const avatar = res.data.map(this.avatar_url => avatar + "\n");
+        // * User name
+        const name = res.data.map(this.login => username + "\n");
+        //   * User GitHub profile
+        const profileLink = res.data.map(this.url => link + "\n");
+        //   * User blog
+        const blogLink = res.data.map(this.blog => blog + "\n");
+        //   * User location via Google Maps
+        const userLocation = res.data.map(this.location => location + "\n");
+        //   * User bio
+        const userbio = res.data.map(this.bio => biography + "\n");
+        //   * Number of public repositories
+        const pubRepos = res.data.map(this.public_repos => publicrepos + "\n");
+        //    * Number of followers
+        const userFollowers = res.data.map(this.followers => followers + "\n");
+        //    * Number of GitHub stars
+        const userStars= res.data.map(this.starred_url => starred + "\n");
+        //     * Number of users following
+        const userFollowing = res.data.map(this.following => following + "\n");
 
 
-// * Links to the following:
-//   * User location via Google Maps
-//   * User GitHub profile
-//   * User blog
-
-// * User bio
-
-// * Number of public repositories
-
-// * Number of followers
-
-// * Number of GitHub stars
-
-// * Number of users following
+    fs.writeFile("repos.pdf", repoNames, "utf-8", (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(
+            `Everything went super great. You did it. Your the best, ever! You saved ${repoNames.length}`
+        );
+    });
+    });
+  });
 
 function writeToFile(fileName, data) {
- 
+
 }
 
 function init() {
 
-init();
+    init();
 };
-
-// * The generated resume includes a bio image from the user's GitHub profile.
-
-// * The generated resume includes the user's location and a link to their GitHub profile.
-
-// * The generated resume includes the number of: public repositories, followers, GitHub stars and following count.
 
 // * The background color of the generated PDF matches the color that the user provides.
